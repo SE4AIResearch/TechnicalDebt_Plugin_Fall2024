0736b0cf90bd40709cd6f4616800070a0d87c65c
Konstantinos Mokos <konstantinosmokos157@gmail.com>
2024-11-04 16:55:59 -0500

    Update SATDToolWindowFactory

--- src/main/Java/com/github/SE4AIResearch/technicaldebt_plugin_fall2024/toolWindow/SATDToolWindowFactory.java
+++ src/main/Java/com/github/SE4AIResearch/technicaldebt_plugin_fall2024/toolWindow/SATDToolWindowFactory.java
@@ -1,5 +1,6 @@
 package com.github.SE4AIResearch.technicaldebt_plugin_fall2024.toolWindow;
 
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,6 +17,10 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.*;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
 
 public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
 
@@ -24,12 +29,12 @@ public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
         JPanel toolWindowPanel = new JPanel();
         toolWindowPanel.setLayout(new BoxLayout(toolWindowPanel, BoxLayout.Y_AXIS)); // Set vertical layout for new lines
 
-        JLabel label = new JLabel("Connecting to SATD database...");
-        toolWindowPanel.add(label);
+        JBLabel label = new JBLabel("Connecting to SATD database...");
+        toolWindowPanel.add(label, BorderLayout.CENTER);
 
-        JScrollPane scrollPane = new JScrollPane(toolWindowPanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // Always show vertical scroll bar
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        //JScrollPane scrollPane = new JScrollPane(toolWindowPanel);
+        //scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // Always show vertical scroll bar
+        //scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
         //TODO: Get working Scrollbar
 
@@ -41,7 +46,7 @@ public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
         toolWindow.getContentManager().addContent(content);
     }
 
-    public static void connectToDatabase(JPanel toolWindowPanel, JLabel label) {
+    public static void connectToDatabase(JPanel toolWindowPanel, JBLabel label) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // This will throw an exception if the driver is not found
             System.out.println("Driver loaded successfully.");
@@ -63,17 +68,23 @@ public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
             ResultSet rs = stmt.executeQuery(query);
 
             // Displaying query results
+            String record = "";
             while (rs.next()) {
-                String record = rs.getString("f_comment"); // Replace with actual column name
-                JLabel resultLabel = new JLabel("Comment: " + record);
-                toolWindowPanel.add(resultLabel);
+                record += "Comment: ";
+                record += rs.getString("f_comment"); // Replace with actual column name
+                record += '\n';
+                //JBLabel resultLabel = new JBLabel("Comment: " + record);
+                //toolWindowPanel.add(resultLabel);
             } 
+
+            label.setText(record);
+            JBScrollPane scrollPane = new JBScrollPane(label, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            toolWindowPanel.add(scrollPane, BorderLayout.CENTER);
 
             // Close resources
             rs.close();
             stmt.close();
             conn.close();
-
         } catch (Exception e) {
             label.setText("Connection failed: " + e.getMessage());
         }
