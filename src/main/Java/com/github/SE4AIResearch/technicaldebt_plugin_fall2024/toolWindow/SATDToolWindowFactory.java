0764a349e3badd2403b89dd6e0e9bad3f798249f
schunduru256 <146134857+schunduru256@users.noreply.github.com>
2024-11-04 17:05:12 -0500

    Update SATDToolWindowFactory.java
    Creating table within the toolwindow

--- src/main/Java/com/github/SE4AIResearch/technicaldebt_plugin_fall2024/toolWindow/SATDToolWindowFactory.java
+++ src/main/Java/com/github/SE4AIResearch/technicaldebt_plugin_fall2024/toolWindow/SATDToolWindowFactory.java
@@ -13,40 +13,42 @@ import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+
 import javax.swing.*;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBPanel;
+import javax.swing.table.DefaultTableModel;
 
 public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        JPanel toolWindowPanel = new JPanel();
-        toolWindowPanel.setLayout(new BoxLayout(toolWindowPanel, BoxLayout.Y_AXIS)); // Set vertical layout for new lines
+        JPanel toolWindowPanel = new JBPanel();
+        toolWindowPanel.setLayout(new BorderLayout());
 
         JBLabel label = new JBLabel("Connecting to SATD database...");
-        toolWindowPanel.add(label, BorderLayout.CENTER);
+        toolWindowPanel.add(label, BorderLayout.NORTH);
 
-        //JScrollPane scrollPane = new JScrollPane(toolWindowPanel);
-        //scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // Always show vertical scroll bar
-        //scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        // Create a table model with column names
+        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.addColumn("Comment Number");
+        tableModel.addColumn("Comment");
 
-        //TODO: Get working Scrollbar
+        // Create the JTable with the model
+        JTable table = new JTable(tableModel);
+        JBScrollPane scrollPane = new JBScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        toolWindowPanel.add(scrollPane, BorderLayout.CENTER);
 
-        connectToDatabase(toolWindowPanel, label);
+        connectToDatabase(tableModel, label);
 
-        //Adds our panel to Intellij's content factory
+        // Adds our panel to IntelliJ's content factory
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(toolWindowPanel, "", false);
         toolWindow.getContentManager().addContent(content);
     }
 
-    public static void connectToDatabase(JPanel toolWindowPanel, JBLabel label) {
+    public static void connectToDatabase(DefaultTableModel tableModel, JBLabel label) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // This will throw an exception if the driver is not found
             System.out.println("Driver loaded successfully.");
@@ -57,8 +59,8 @@ public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
             // Establish a connection to the MySQL database
             String url = "jdbc:mysql://localhost:3306/satd"; // Update with your database name
             String user = "root"; // Replace with your MySQL username
-            String password = "1Sow74902hope"; // Replace with your MySQL password
-            
+            String password = "Srasg256"; // Replace with your MySQL password
+
             Connection conn = DriverManager.getConnection(url, user, password);
             label.setText("Connection successful!");
 
@@ -68,18 +70,11 @@ public class SATDToolWindowFactory implements ToolWindowFactory, DumbAware {
             ResultSet rs = stmt.executeQuery(query);
 
             // Displaying query results
-            String record = "";
+            int commentNumber = 1; // Initialize comment number
             while (rs.next()) {
-                record += "Comment: ";
-                record += rs.getString("f_comment"); // Replace with actual column name
-                record += '\n';
-                //JBLabel resultLabel = new JBLabel("Comment: " + record);
-                //toolWindowPanel.add(resultLabel);
-            } 
-
-            label.setText(record);
-            JBScrollPane scrollPane = new JBScrollPane(label, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-            toolWindowPanel.add(scrollPane, BorderLayout.CENTER);
+                String f_comment = rs.getString("f_comment"); // Replace with actual column name
+                tableModel.addRow(new Object[]{commentNumber++, f_comment}); // Add new row to the table
+            }
 
             // Close resources
             rs.close();
