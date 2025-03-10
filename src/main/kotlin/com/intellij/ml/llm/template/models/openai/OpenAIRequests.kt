4f0b3a7dece6314a6c585cc142ffdfeead4ad802
kulnel <nkulkar5@stevens.edu>
2025-03-10 02:08:38 -0400

    Finalized the LLM tool window

--- src/main/kotlin/com/intellij/ml/llm/template/models/openai/OpenAIRequests.kt
+++ src/main/kotlin/com/intellij/ml/llm/template/models/openai/OpenAIRequests.kt
@@ -15,7 +15,6 @@ open class OpenAIBaseRequest<Body>(path: String, body: Body) : LLMBaseRequest<Bo
         val apiKey = CredentialsHolder.getInstance().getOpenAiApiKey()?.ifEmpty { null }
             ?: throw AuthorizationException("OpenAI API Key is not provided")
 
-        println("Here: $apiKey")
 
         return HttpRequests.post(url, "application/json")
             .tuner {
